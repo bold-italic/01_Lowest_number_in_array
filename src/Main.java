215287b8c589bbbbfaf82b910ff0fd5ab9f0177e
Sergey Varlamov <msisdn.com@gmail.com>
2024-06-19 22:46:54 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] numbers = {20, 22, 56, 48, 18, 29, 3, 30};
+        int lowestNumber = numbers[0];
+        for (int number : numbers) {
+            if (number < lowestNumber) {
+                lowestNumber = number;
+            }
+        }
+        System.out.println("The lowest number in the array is: " + lowestNumber);
+    }
+}
